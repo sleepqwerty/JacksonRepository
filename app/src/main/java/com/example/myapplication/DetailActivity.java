3168689e19f0223ad0468d1b52b8d56e56d33f94
Jackson Li <jacksonli773@gmail.com>
2020-09-29 19:51:55 +1000

    week 3 tutorial

--- app/src/main/java/com/example/myapplication/DetailActivity.java
+++ app/src/main/java/com/example/myapplication/DetailActivity.java
@@ -12,28 +12,51 @@ import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
     public static final String INTENT_MESSAGE = "au.edu.unsw.infs3634.covidtracker.intent_message";
-    private String TAG = "DETAIL_ACTIVITY";
+
+    private TextView mCountry;
+    private TextView mNewCases;
+    private TextView mTotalCases;
+    private TextView mNewDeaths;
+    private TextView mTotalDeaths;
+    private TextView mNewRecovered;
+    private TextView mTotalRecovered;
+    private TextView mSearch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        Log.d(TAG, "onCreate: Line 18");
+        //Log.d(TAG, "onCreate: Line 18");
         //get the intent
-        Intent intent = getIntent();
+        final Intent intent = getIntent();
+
+        String countryCode = intent.getStringExtra(INTENT_MESSAGE);
         //find tvDetailMessage TextView
-        TextView detailMessage = findViewById(R.id.tvDetail);
+        final au.edu.unsw.infs3634.covidtracker.Country country = au.edu.unsw.infs3634.covidtracker.Country.getCountry(countryCode);
+
+        mCountry = findViewById(R.id.tvCountry);
+        mNewCases = findViewById(R.id.tvNewCases);
+        mTotalCases = findViewById(R.id.tvTotalCases);
+        mNewDeaths = findViewById(R.id.tvNewDeaths);
+        mTotalDeaths = findViewById(R.id.tvTotalDeaths);
+        mNewRecovered = findViewById(R.id.tvNewRecovered);
+        mTotalRecovered = findViewById(R.id.tvTotalRecovered);
         // get the message from the intent and set the value to the TextView element
-        detailMessage.setText(intent.getStringExtra(INTENT_MESSAGE));
-        Button button = findViewById(R.id.btnShowVideo);
-        button.setOnClickListener(new View.OnClickListener(){
+        mCountry.setText(country.getCountry());
+        mNewCases.setText(String.valueOf(country.getNewConfirmed()));
+        mTotalCases.setText(String.valueOf(country.getTotalConfirmed()));
+        mNewDeaths.setText(String.valueOf(country.getNewDeaths()));
+        mTotalDeaths.setText(String.valueOf(country.getTotalDeaths()));
+        mNewRecovered.setText(String.valueOf(country.getNewRecovered()));
+        mTotalRecovered.setText(String.valueOf(country.getTotalRecovered()));
+        // tv.setText(country.getCountry());
+        mSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openYoutube();
+                Intent webintent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/search?q=covid " + country.getCountry()));
+                startActivity(webintent);
             }
         });
-    }
-    public void openYoutube() {
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=Tlwda9S58Lg&ab_channel=Fireballfury"));
-        startActivity(intent);
-    }
-}
+    }}
+        // Button button = findViewById(R.id.btnShowVideo);
+       // button.setOnClickListener(new View.OnClickListener(){
+
