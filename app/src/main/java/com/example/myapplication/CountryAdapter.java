a2c408ce2f5fa3c540f3099935e902f867a17fb3
Jackson Li <jacksonli773@gmail.com>
2020-10-13 18:09:25 +1100

    week 4 tutorial

---
+++ app/src/main/java/com/example/myapplication/CountryAdapter.java
@@ -0,0 +1,57 @@
+package com.example.myapplication;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.CountryViewHolder> {
+    private ArrayList<Country> mCountries;
+    private Listener mListener;
+    public CountryAdapter(ArrayList<Country> countries, Listener listener) {
+        mCountries = countries;
+        mListener = listener;
+    }
+    public interface Listener {
+        void onClick(View view,String countryCode);
+    }
+    @NonNull
+    @Override
+    public CountryAdapter.CountryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.country_list_row, parent, false);
+        CountryViewHolder holder = new CountryViewHolder(v, mListener);
+        return holder;
+    }
+    @Override
+    public void onBindViewHolder(@NonNull CountryAdapter.CountryViewHolder holder, int position) {
+        Country country = mCountries.get(position);
+        DecimalFormat df = new DecimalFormat("#,###,###,###");
+        holder.country.setText(country.getCountry());
+        holder.totalCases.setText(df.format(country.getTotalConfirmed()));
+        holder.newCases.setText(df.format(country.getNewConfirmed()));
+        holder.itemView.setTag(country.getCountryCode());
+    }
+    @Override
+    public int getItemCount() {
+        return mCountries.size();
+    }
+    public class CountryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        public TextView country, totalCases, newCases;
+        private Listener listener;
+        public CountryViewHolder(@NonNull View itemView, Listener listener) {
+            super(itemView);
+            this.listener = listener;
+            itemView.setOnClickListener(this);
+            country = itemView.findViewById(R.id.tvCountry);
+            totalCases = itemView.findViewById(R.id.tvTotalCases);
+            newCases = itemView.findViewById(R.id.tvNewCases);
+        }
+        @Override
+        public void onClick(View v) {
+            listener.onClick(v, (String) v.getTag());
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,34 +1,39 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 public class MainActivity extends AppCompatActivity {
-
-
+    private RecyclerView mRecyclerView;
+    private CountryAdapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.btnLaunchActivity);
-        button.setOnClickListener(new View.OnClickListener() {
+        mRecyclerView = findViewById(R.id.rvList);
+        mRecyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+        CountryAdapter.Listener listener = new CountryAdapter.Listener() {
             @Override
-            public void onClick(View v) {
-                LaunchDetailActivity("US");
+            public void onClick(View view, String countryCode) {
+                launchDetailActivity(countryCode);
             }
-        });
+        };
+        mAdapter = new CountryAdapter(Country.getCountries(), listener);
+        mRecyclerView.setAdapter(mAdapter);
     }
-
-
-
-    private void LaunchDetailActivity(String message) {
-                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
-                //intent.putExtra(DetailActivity.INTENT_MESSAGE, message);
-                intent.putExtra(DetailActivity.INTENT_MESSAGE, message);
-                startActivity(intent);
-            }
-            }
+    private void launchDetailActivity(String message){
+        // Declare an intent variable to launch Detail Activity on click
+        Intent intent = new Intent( MainActivity.this, DetailActivity.class);
+        // Pass a message to DetailActivity via intent variable
+        intent.putExtra(DetailActivity.INTENT_MESSAGE, message);
+        //start the DetailActivity
+        startActivity(intent);
+    }
+}
